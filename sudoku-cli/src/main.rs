4a3e3939cd4b096ff36ed110aaadfcaa9c7cdbde
LucasHazardous <70599201+LucasHazardous@users.noreply.github.com>
2023-10-01 20:10:54 +0200

    reformat and optimize sudoku-cli

--- sudoku-cli/src/main.rs
+++ sudoku-cli/src/main.rs
@@ -1,53 +1,35 @@
 use cursive::view::{Resizable, Nameable};
 use cursive::{Cursive, CursiveExt};
 use cursive::views::{EditView, Dialog, LinearLayout, NamedView, ResizedView, TextView};
+
 use sudoku_utils::generating::generate_board;
 use sudoku_utils::validating::validate;
 
-type SudokuInput = NamedView<ResizedView<ResizedView<ResizedView<ResizedView<EditView>>>>>;
-type SudokuText = ResizedView<ResizedView<TextView>>;
-
-enum SudokuElement {
-    Input(SudokuInput),
-    Text(SudokuText)
-}
+type SudokuInput = NamedView<ResizedView<ResizedView<EditView>>>;
 
 fn main() {
     let board = generate_board(1);
 
     let mut siv = Cursive::default();
 
-    let mut grid = Vec::new();
+    let mut grid_layout = LinearLayout::vertical();
+
     for i in 0..9 {
-        let mut row: Vec<SudokuElement> = Vec::new();
+        let mut row_layout = LinearLayout::horizontal();
+
         for j in 0..9 {
             if board[i][j] == 0 {
                 let edit_view = EditView::new()
                 .max_content_width(1)
-                .min_width(3)
-                .max_width(3)
-                .min_height(1)
-                .max_height(1)
+                .fixed_width(3)
+                .fixed_height(1)
                 .with_name(format!("cell_{}_{}", i, j));
-                row.push(SudokuElement::Input(edit_view));
+                row_layout.add_child(edit_view);
             } else {
                 let text_view = TextView::new(board[i][j].to_string())
                 .fixed_width(3)
                 .fixed_height(1);
-                row.push(SudokuElement::Text(text_view))
-            }
-
-        }
-        grid.push(row);
-    }
-
-    let mut grid_layout = LinearLayout::vertical();
-    for row in grid {
-        let mut row_layout = LinearLayout::horizontal();
-        for cell in row {
-            match cell {
-                SudokuElement::Input(field) => row_layout.add_child(field),
-                SudokuElement::Text(text) => row_layout.add_child(text)
+                row_layout.add_child(text_view);
             }
         }
         grid_layout.add_child(row_layout);
@@ -61,31 +43,36 @@ fn main() {
     let dialog = Dialog::new()
         .title("Sudoku Game")
         .content(layout)
-        .button("Validate", |s| {
-            let mut sboard = s.user_data::<[[u8; 9]; 9]>().unwrap().clone();
-            for i in 0..9 {
-                for j in 0..9 {
-                    if sboard[i][j] != 0 {
-                        continue;
-                    }
-                    sboard[i][j] = s.call_on_name(&format!("cell_{}_{}", i, j), |view: &mut SudokuInput| {
-                        let content = view.get_mut().get_inner().get_inner().get_inner().get_inner().get_content().to_string();
-                        if content.len() > 0 {
-                            content.parse().unwrap()
-                        } else {
-                            0
-                        }
-                    }).unwrap();
-                }
-            }
-
-            if validate(&sboard) {
-                s.add_layer(Dialog::around(TextView::new("You won")).button("Quit", |ss| ss.quit()));
-            }
-        })
+        .button("Validate", validate_board)
         .button("Quit", |s| s.quit());
 
     siv.add_layer(dialog);
 
     siv.run();
+}
+
+fn validate_board(s: &mut Cursive) {
+    let mut sboard = s.user_data::<[[u8; 9]; 9]>().unwrap().clone();
+    for i in 0..9 {
+        for j in 0..9 {
+            if sboard[i][j] != 0 {
+                continue;
+            }
+            sboard[i][j] = s.call_on_name(&format!("cell_{}_{}", i, j), |view: &mut SudokuInput| {
+                let content = view.get_mut().get_inner().get_inner().get_content().to_string();
+                if content.len() > 0 {
+                    content.parse().unwrap_or(0)
+                } else {
+                    0
+                }
+            }).unwrap();
+        }
+    }
+
+    if validate(&sboard) {
+        s.add_layer(Dialog::around(
+            TextView::new("You won"))
+            .button("Quit", |ss| ss.quit()
+        ));
+    }
 }
